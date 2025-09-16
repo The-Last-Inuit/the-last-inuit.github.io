ed81d54c7baef09e574258f547e3ead31296d6f8
max feinberg <me@thelastinu.it>
2025-09-16 21:20:43 +0300

    update website.

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,6 @@ import Config
 config :still,
   dev_layout: false,
   input: Path.join(Path.dirname(__DIR__), "priv/site"),
-  output: Path.join(Path.dirname(__DIR__), "_site")
+  output: Path.join(Path.dirname(__DIR__), ".")
 
 import_config("#{Mix.env()}.exs")
--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+mix still.compile
 git add .
-mv _site/* .
 git commit -m "update website".
 git push -f

0036a15e8879b2cf0d79983a5890305e611daa56
max feinberg <me@thelastinu.it>
2025-09-27 23:02:29 +0300

    wip

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ import Config
 config :still,
   dev_layout: false,
   input: Path.join(Path.dirname(__DIR__), "priv/site"),
-  output: Path.join(Path.dirname(__DIR__), ".")
+  output: Path.join(Path.dirname(__DIR__), "."),
+  pass_through_copy: [~r/.*\.png/]
 
 import_config("#{Mix.env()}.exs")

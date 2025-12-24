b2c55b889fbbe464642ee7c4368555bf17cba39f
noumena hundimägi <myself@noumena.mx>
2025-12-24 03:14:57 +0200

    update website.

--- push.sh
+++ push.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
+zola build
+rm -rf ./about
+rm -rf ./icon
+rm -rf ./img
+rm -rf ./js
+rm -rf ./posts
+mv public/* .
 git add .
 git commit -m "update website".
 git push -f

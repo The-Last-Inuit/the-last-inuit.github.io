e3199771b1ec95f7ef71ba3f966e67e3c6becfcb
noumena hundimägi <myself@noumena.mx>
2026-01-23 06:49:32 +0200

    update website.

--- push.sh
+++ push.sh
@@ -10,6 +10,7 @@ rm -rf ./posts
 rm -rf ./projects
 mv public/* .
 rm -rf public
+for f in *.html; do pandoc "$f" -t plain -o "${f%.html}.txt"; done
 git add .
 git commit -m "update website".
 git push -f
